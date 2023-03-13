16efa98d472ed949982c1a406cd01657403a7dad
seb-art <kimanzikims300@gmail.com>
2023-03-13 01:32:17 -0700

    replaces an element with another

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for(idx in range(len(my_list)):
+            if(idx < 0):
+                None
+            elif(idx >= len(my_list):
+                return None
+             else:
+                return my_list[idx]
