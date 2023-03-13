0d6823fe08573a1e046c227518e8ffbe8ec7076f
seb-art <kimanzikims300@gmail.com>
2023-03-13 03:52:57 -0700

    finds max int in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
-        return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
 
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    else:
+        max_int = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max_int:
+                max_int = my_list[i]
+        return (max_int)
