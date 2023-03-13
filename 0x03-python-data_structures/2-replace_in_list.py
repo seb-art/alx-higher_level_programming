36cb1002c73c84c5a44f7e861358048678286c1b
seb-art <kimanzikims300@gmail.com>
2023-03-13 01:41:29 -0700

    replaces element with another

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
     if(idx < 0):
-        None
-    elif(idx >= len(my_list):
+        return None
+    if(idx >= len(my_list):
             return None
     return my_list[idx]
