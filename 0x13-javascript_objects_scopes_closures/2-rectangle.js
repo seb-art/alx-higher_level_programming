5dd75c9d3f0e7bb8f3e743b0d9ae39f5c9f81e9f
seb-art <kimanzikims300@gmail.com>
2023-06-13 14:05:01 -0700

    create an empty object given a condition

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+	constructor(w, h) {
+		this.width = w;
+		this.height = h;
+	}
+};
+
+if (w === 0 || h === 0 && w < 0 || h < 0) {
+	const new Object();
+}
