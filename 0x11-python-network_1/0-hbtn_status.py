39447f7f946217c79a66be8d92554a8d72c11935
seb-art <kimanzikims300@gmail.com>
2023-08-25 11:39:17 -0700

    task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
+'''module:
+a script that fetches url resources
+'''
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as res:
+    body = res.read()
+
+print('Body response:')
+print('\t- type:', type(body))
+print('\t- content:', body)
+print('\t- utf8 content:', body.decode('utf-8'))
